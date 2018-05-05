9bb1fc48273a574cc275afc36fde163f102f604b
ismayatikiran <37750299+ismayatikiran@users.noreply.github.com>
2018-05-05 09:55:05 -0400

    2nd New Test Commit

---
+++ UnitedHealthCare/src/test/java/testUnitedHealthCare/LearnMoretest.java
@@ -0,0 +1,14 @@
+package testUnitedHealthCare;
+
+import mainUnitedHealthCare.LearnMore;
+import org.testng.annotations.Test;
+
+public class LearnMoretest extends LearnMore {
+
+    @Test
+    public void VirtualVisittest(){
+        VirtualVisit();
+
+    }
+
+}
---
+++ UnitedHealthCare/src/test/java/testUnitedHealthCare/LogonMyaccountTest.java
@@ -0,0 +1,15 @@
+package testUnitedHealthCare;
+
+import mainUnitedHealthCare.LogonMyaccount;
+import org.testng.annotations.Test;
+
+public class LogonMyaccountTest extends LogonMyaccount {
+
+    @Test
+    public void uchLoginTest(){
+        uhcLogin();
+
+
+    }
+
+}
